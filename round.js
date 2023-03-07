b0e61771332afe6603c711e855333d1b8aa8fc6a
dennis.muthuri <denmurithi@gmail.com>
2023-03-07 09:49:44 +0300

    learn math methods in js

---
+++ round.js
@@ -0,0 +1,6 @@
+
+let num = 10.9
+console.log(Math.round(num))// rounds num if is .5 or above in decimal notation.
+console.log(Math.floor(num)); // returns whole number as it is(no round off/ignores decimals)
+console.log(Math.ceil(num));// round num if has .01 or .1 and above to nearest whole
+console.log(Math.sign(num))// returns +ve or -ve according to the num
